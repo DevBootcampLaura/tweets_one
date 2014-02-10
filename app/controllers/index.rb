b19da5846c0d8828f68fe0995a9a42d9a6fc49a0
Jacky <jsum528@icloud.com>
2014-02-10 15:37:08 -0600

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,21 +2,26 @@ get '/' do
 
 end
 
+get '/stale' do
+  @user = TwitterUser.find_by_username(params[:username])
+  @user.tweets << TwitterUser.fetch_tweets!(@user.username)
+  @tweets = @user.tweets.limit(10)
+  erb :_view_tweets
+
+end
+
 get '/:username' do
-
-  # @user = TwitterUser.find_by_username(params[:username])
-  # if @user.tweets.empty?
-  #   @user.tweets << TwitterUser.fetch_tweets!(@user.username)
-  # end
-  # @tweets = @user.tweets.limit(10)
-
 
   @user = TwitterUser.find_by_username(params[:username])
   if @user.tweets_stale?
     @user.tweets << TwitterUser.fetch_tweets!(@user.username)
-  end
-  @tweets = @user.tweets.limit(10)
 
-  erb :tweets
+    erb :stale
+  else
+    @tweets = @user.tweets.limit(10)
+    erb :tweets
+  end
 
 end
+
+
